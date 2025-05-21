b8f5ba084ef0c9155269e8c8207e36ee23d7429c
ALDY <foxeesaga12@gmail.com>
2025-05-22 02:02:52 +0700

    ok

---
+++ BE/repository/auth.js
@@ -0,0 +1,9 @@
+// atur database seperti masuk ke mana
+/**
+ *  * Important: This file is a placeholder for the authentication repository.
+ *  ! error: This file should not be used in production.
+ * ? what : This file is a placeholder for the authentication repository.
+ * ? why : This file is a placeholder for the authentication repository.
+ * TODO : This file should be replaced with the actual authentication repository.
+ * @param {string} email - The email address of the user.
+ */
