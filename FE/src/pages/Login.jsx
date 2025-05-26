749a0f53809b45fcd5c0595d5e4600fa231dbec8
Valentio <valentiocoding@gmail.com>
2025-05-26 20:18:19 +0700

    Update Login.jsx

--- FE/src/pages/Login.jsx
+++ FE/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { decodeToken } from "react-jwt";
+import logo from "../assets/logo.png"
 
 const Login = () => {
   
@@ -37,17 +38,22 @@ const navigate = useNavigate();
       <div>
         {/* Logo bisa dipasang jika ada */}
       </div>
-      <div className="bg-white text-blue p-8 rounded-xl shadow-2xl w-full max-w-md font-montserrat">
+      <div className="bg- text-blue p-8 rounded-xl shadow-2xl w-full max-w-md font-montserrat items-center">
+        
+        <div className="flex justify-center pb-2"><img src={logo} width={300} alt="" /></div>
         <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
         <div className="space-y-4">
           <div>
+            <form action="">
+              
+            </form>
             <label htmlFor="username" className="block mb-1 text-sm">
               Username
             </label>
             <input
               type="text"
               id="username"
-              placeholder="example@mail.com"
+              placeholder="Input your username"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
               className="w-full px-4 py-2 rounded-md text-blue border border-blue focus:outline-none focus:ring-2 focus:ring-[#FFD700]"
