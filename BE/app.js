3db5726591c9b171e74830afdbc2b7f55d9a05b1
ALDY <foxeesaga12@gmail.com>
2025-05-29 13:43:26 +0700

    hello

--- BE/app.js
+++ BE/app.js
@@ -17,5 +17,10 @@ app.use(express.json());
 // Gunakan routes
 app.use("/", authRoutes);
 app.use("/transaction", transactionRoutes);
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to the Expense Tracker API",
+  });
+});
 
 app.listen(5000, () => console.log("Server running on port 5000"));
